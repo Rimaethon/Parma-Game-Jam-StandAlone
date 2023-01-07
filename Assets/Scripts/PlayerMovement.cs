5d9aacefbc1d3bd71cafaf608b74610293e8f739
LordfireTR <lordfiretr@gmail.com>
2023-01-07 23:56:30 +0300

    Revert "Movement commit"
    This reverts commit dd04cdfbb4c769bf20ed0ffacf37788a4f0ce70b.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,31 +6,41 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     Rigidbody playerRb;
-    PlayerInput playerInput;
+    PlayerInput PlayerInput;
 
     bool isGrounded, canJump;
-    Vector3 movementInput;
-    float jumpForce = 400f, moveForce = 1000f, maxSpeed = 10f;
+
+    float jumpForce = 400f, moveForce = 1000f;
 
     void Awake()
     {
         playerRb = GetComponent<Rigidbody>();
-        playerInput = GetComponent<PlayerInput>();
-        
         canJump = true;
+        PlayerInput = GetComponent<PlayerInput>();
     }
 
-    void FixedUpdate()
+    void OnEnable()
+    {
+        PlayerInput.onActionTrigger += PlayerInputActionTriggered;
+    }
+
+    public void PlayerInputActionTriggered(InputAction.CallbackContext context)
+    {
+        if (context.action.name == "Movement")
+        {
+            Movement(context);
+        }
+    }
+
+    void Update()
     {
         CheckGrounded();
-        HandleMovement();
     }
 
     public void Jump(InputAction.CallbackContext context)
     {
-        //Standard jump. Coroutine sets canJump to TRUE after 0.1 seconds. 
-        //CheckGrounded() uses raycast to check if the character is grounded.
-        //We can use a simple state machine as well but I think it isn't necessary for now
+        Debug.Log(isGrounded + "," + canJump);
+
         if (context.performed && isGrounded && canJump)
         {
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
@@ -42,20 +52,12 @@ public class PlayerMovement : MonoBehaviour
 
     public void Movement(InputAction.CallbackContext context)
     {
-        Vector2 inputVector2 = context.ReadValue<Vector2>();
-        movementInput = new Vector3(inputVector2.x, 0, inputVector2.y);
-    }
-
-    public void HandleMovement()
-    {
-        //Takes the input vector from Movement() to get movement direction. Also using Vector3.ClampMagnitude to set a max speed. 
-        playerRb.AddForce(movementInput * moveForce, ForceMode.Force);
-        playerRb.velocity = Vector3.ClampMagnitude(playerRb.velocity, maxSpeed);
+        playerRb.AddForce(context.ReadValue<Vector2>() * moveForce);
+        Debug.Log(context.ReadValue<Vector2>());
     }
 
     void CheckGrounded()
     {
-        //Using raycast to check if the character is grounded. We can add layer mask if necessary.
         RaycastHit hit;
         Ray landingRay = new Ray(transform.position, Vector3.down);
         if (Physics.Raycast(landingRay, out hit, .1f) && canJump)
@@ -66,7 +68,6 @@ public class PlayerMovement : MonoBehaviour
 
     IEnumerator CheckCanJump()
     {
-        //Jump input has 0.1 seconds cooldown to prevent input duplication.
         yield return new WaitForSeconds(0.1f);
         canJump = true;
     }
